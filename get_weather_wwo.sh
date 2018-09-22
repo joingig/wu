8b04272a1feca6289b70265b317677066981f5e6
root <root@raspberrypi.(none)>
2018-09-22 23:12:15 +0300

    add get for weather world online service

---
+++ get_weather_wwo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wuhome=/root/wu
+#wuhome=/home/tazz/wu
+
+if [ ! -e $wuhome/wwo.key ]; then
+	printf "worldweatheronline key not found in %s \n" $wuhome/wwo.key 
+        logger "$0 worldweatheronline key not found in $wuhome/wwo.key"
+	exit 100
+fi
+wkey=`head $wuhome/wwo.key`
+printf "key : %s\n" $wkey
+/usr/bin/curl "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=$wkey&q=Moscow&format=json&num_of_days=3" -o $wuhome/wwo00.json
+
