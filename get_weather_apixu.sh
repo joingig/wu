d2d53fb836f35d2739131f325fd9956116954525
root <root@raspberrypi.(none)>
2019-02-18 09:25:29 +0300

    add some workaround while donloading json from APIXU

--- get_weather_apixu.sh
+++ get_weather_apixu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#wuhome=/root/wu
-wuhome=/home/tazz/wu
+wuhome=/root/wu
+#wuhome=/home/tazz/wu
 key=$wuhome/apixu.key
 days=2
 
@@ -13,4 +13,19 @@ fi
 wkey=`head $key`
 printf "key : %s\n" $wkey
 /usr/bin/curl "http://api.apixu.com/v1/forecast.json?key=$wkey&q=Moscow&days=$days" -o $wuhome/apixu.json
+
+python -c 'import sys, json, os; wuhome = os.environ.get("wuhome","/root/wu"); data_f = open(wuhome+"/apixu.json","r"); js_obj = json.load(data_f); print js_obj;'
+
+if [ $? -eq 0 ]; then
+    printf "No errors\n"
+    exit 0;
+fi
+
+#we has some errors try after 5 min
+printf "Sleep 5 min\n"
+sleep 5m
+
+/usr/bin/curl "http://api.apixu.com/v1/forecast.json?key=$wkey&q=Moscow&days=$days" -o $wuhome/apixu.json
+
+exit 0
 
